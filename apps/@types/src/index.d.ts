6bf271672f8e7197de08ecf68b2c432d4608f903
ymkz <ymkz@users.noreply.github.com>
2022-11-27 08:38:40 +0000

    chore: back to prettier formatter

--- apps/@types/src/index.d.ts
+++ apps/@types/src/index.d.ts
@@ -1,55 +1,55 @@
 type Env = {
-	kv_ua: KVNamespace;
-	kv_clip: KVNamespace;
-	kv_novel: KVNamespace;
-	ua_manager: Fetcher;
-};
+  kv_ua: KVNamespace
+  kv_clip: KVNamespace
+  kv_novel: KVNamespace
+  ua_manager: Fetcher
+}
 
 type MetaData = {
-	contentType: string;
-};
+  contentType: string
+}
 
 type ResponseChromiumReleases = {
-	channel: "Stable";
-	chromium_main_branch_position: number;
-	hashes: {
-		angle: string;
-		chromium: string;
-		dawn: string;
-		devtools: string;
-		pdfium: string;
-		skia: string;
-		v8: string;
-		webrtc: string;
-	};
-	milestone: number;
-	platform: string;
-	previous_version: string;
-	time: number;
-	version: string;
-}[];
+  channel: 'Stable'
+  chromium_main_branch_position: number
+  hashes: {
+    angle: string
+    chromium: string
+    dawn: string
+    devtools: string
+    pdfium: string
+    skia: string
+    v8: string
+    webrtc: string
+  }
+  milestone: number
+  platform: string
+  previous_version: string
+  time: number
+  version: string
+}[]
 
 type ClipItem = {
-	id: string;
-	url: string;
-	title: string;
-	hasImage: boolean;
-	description?: string;
-};
+  id: string
+  url: string
+  title: string
+  hasImage: boolean
+  description?: string
+}
 
 type ClipImage = {
-	src: ArrayBuffer;
-	contentType: string;
-};
+  src: ArrayBuffer
+  contentType: string
+}
 
 type ClipImageMetaData = {
-	contentType: string;
-};
+  contentType: string
+}
 
 type ClipInfo = {
-	id: string;
-	url: string;
-	title: string;
-	description?: string;
-	image?: ClipImage;
-};
+  id: string
+  url: string
+  title: string
+  description?: string
+  image?: ClipImage
+}
--- apps/clip-api/src/schema.ts
+++ apps/clip-api/src/schema.ts
@@ -1,22 +1,22 @@
-import { Validator } from "hono";
+import { Validator } from 'hono'
 
 export const addSchema = (v: Validator) => ({
-	url: v
-		.json("url")
-		.isRequired()
-		.message(`request body 'url' is required as 'string'`),
-});
+  url: v
+    .json('url')
+    .isRequired()
+    .message(`request body 'url' is required as 'string'`),
+})
 
 export const delSchema = (v: Validator) => ({
-	id: v
-		.json("id")
-		.isRequired()
-		.message(`request body 'id' is required as 'string'`),
-});
+  id: v
+    .json('id')
+    .isRequired()
+    .message(`request body 'id' is required as 'string'`),
+})
 
 export const imageSchema = (v: Validator) => ({
-	key: v
-		.query("key")
-		.isRequired()
-		.message(`request body 'key' is required as 'string'`),
-});
+  key: v
+    .query('key')
+    .isRequired()
+    .message(`request body 'key' is required as 'string'`),
+})
