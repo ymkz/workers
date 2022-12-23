e18b5398afa12c4a3537f2251b2bd95149064ff3
ymkz <ymkz@users.noreply.github.com>
2022-12-23 16:21:19 +0000

    chore: use new cloudflare adapter

--- apps/clip-web/vite.config.ts
+++ apps/clip-web/vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 
-/** @type {import('vite').UserConfig} */
-const config = {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   plugins: [sveltekit()],
   server: { port: 3000 },
-}
-
-export default config
+})
