04f1927f206637ac532cd44145a7d176b423876f
ymkz <ymkz@users.noreply.github.com>
2023-01-08 13:31:40 +0000

    chore: modify dev command

--- apps/clip-web/src/routes/+page.svelte
+++ apps/clip-web/src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script>
+	let key;
+	let code;
+
+	function handleKeydown(event) {
+		console.log("blur")
+	}
+</script>
+
+<svelte:window on:blur={handleKeydown}/>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	p {
+		color: purple;
+	}
+</style>
